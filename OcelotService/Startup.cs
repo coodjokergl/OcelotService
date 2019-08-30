eec812e23aafcfbae7cdf05ef53ce7b3debb3429
龚磊 <Gongl01@mingyuanyun.com>
2019-08-30 11:16:04 +0800

    增加Consul发现服务

--- OcelotService/Startup.cs
+++ OcelotService/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 
 namespace OcelotService
 {
@@ -20,8 +21,8 @@ namespace OcelotService
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddJsonFile("ocelot.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true,reloadOnChange:true)
+                .AddJsonFile("ocelot.json" ,optional: true,reloadOnChange:true)
                 .AddEnvironmentVariables();
 
             Configuration = builder.Build();
@@ -34,7 +35,11 @@ namespace OcelotService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot(Configuration);
+            services
+                .AddOcelot(Configuration)
+                .AddConsul()
+                //.AddConfigStoredInConsul()
+                ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
